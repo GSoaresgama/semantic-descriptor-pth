f3fe9231503a3437addeb61c4f1bb6558bd4dc35
GSoaresgama <gabrielsgamma@gmail.com>
2022-02-21 18:28:54 -0300

    data

--- data.py
+++ data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import pylab
-
+# import scipy
+from scipy import stats, optimize, interpolate
 my_data_sem = pd.Series([1.270882,
 1.541523,
 1.553915,
@@ -24,10 +25,23 @@ my_data = pd.Series([5.188256,
 5.168882,
 1.372640])
 
-data = pd.concat([my_data, my_data_sem], axis=1)
 
+# res = stats.normaltest(my_data_sem, axis=0, nan_policy='propagate')
+# ORB = stats.skewtest(my_data, axis=0, nan_policy='propagate')
+# ORB_sem = stats.skewtest(my_data_sem, axis=0, nan_policy='propagate')
+ORB = stats.normaltest(my_data, axis=0, nan_policy='propagate')
+ORB_sem = stats.normaltest(my_data_sem, axis=0, nan_policy='propagate')
+
+# res = stats.kstest(my_data_sem, 'norm')
+print("ORB: ",ORB)
+print("ORB_sem: ",ORB_sem)
+testT = stats.ttest_ind(my_data,my_data_sem)
+print("testT: ",testT)
+# data = pd.concat([my_data, my_data_sem], axis=1)
+kruskal = stats.kruskal(my_data_sem, my_data)
+print("kruskal: ", kruskal)
 # my_data.hist()
 # my_data.plot()
-data.plot(kind = 'box')
+# data.plot(kind = 'box')
 
-pylab.show()
+# pylab.show()
